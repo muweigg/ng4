0eec5d6b57920c4fac8e966f1fb335571a391ea5
Wei Mu <Wei Mu>
2017-06-19 22:19:15 +0800

    update

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'home',
@@ -6,21 +6,9 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-    @ViewChild('nameInput') nameInput:ElementRef;
     title:String = "ng4-start";
-    names:Array<any> = [];
-    name:String = "";
 
     constructor() { }
 
     ngOnInit() { }
-
-    addItem() {
-        console.log(this.name);
-        if (this.name.length > 0) {
-            this.names.push(this.name);
-            this.name = '';
-            this.nameInput.nativeElement.focus();
-        }
-    }
 }
